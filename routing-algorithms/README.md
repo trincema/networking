673a82d8efe6d0450441637ca2051f18e7df9e20
Emanuel TRINC <emanuel.trinc@hoprnet.org>
2023-09-18 08:09:57 +0300

    bkp

--- routing-algorithms/README.md
+++ routing-algorithms/README.md
@@ -1,1 +1,2 @@
 ## Dijkstra's Shortest Path Algorithm
+![image-121](https://github.com/trincema/networking/assets/7762113/e770c814-440f-437d-8b85-5e162fc03ce2)
---
+++ routing-algorithms/routing.cpp
@@ -0,0 +1,16 @@
+// https://thinkgeo.com/blog/dijkstras-routing-algorithm
+#include <iostream>
+using namespace std;
+
+struct Node {
+    
+};
+
+class DijkstrasShortestPath {
+private:
+    Node* topology;
+};
+
+int main() {
+
+}
